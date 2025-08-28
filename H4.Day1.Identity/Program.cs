b50b596c61d1d87530c5efdcee0fe8ebe075cbb2
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2025-08-28 11:12:34 +0200

    chore: setup di file to make program.cs more readable

--- H4.Day1.Identity/Program.cs
+++ H4.Day1.Identity/Program.cs
@@ -1,11 +1,8 @@
-using System.Security.Authentication;
-using H4.Day1.Identity.Codes;
+using H4.Day1.Identity;
 using H4.Day1.Identity.Components;
 using H4.Day1.Identity.Components.Account;
-using H4.Day1.Identity.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,62 +22,15 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy("AuthenticatedUser", policy =>
-    {
-        policy.RequireAuthenticatedUser();
-    })
-    .AddPolicy("RequireAdminRole", policy =>
-    {
-        policy.RequireRole("Admin");
-    });
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequiredLength = 8;
-});
-
-var todoConnectionString = builder.Configuration.GetConnectionString("TodoDbConnection") ?? throw new InvalidOperationException("Connection string 'TodoDbConnection' not found.");
-builder.Services.AddDbContext<TodoDbContext>(options =>
-    options.UseSqlServer(todoConnectionString));
-
-// Ikke nødvendigt da Kestrel automatisk bruger appsettings, secrets etc. så længe paths er sat rigtigt op
-// Men beholdt som eksempel.
-
-//builder.WebHost.UseKestrel(((context, options) =>
-//{
-//    options.Configure(context.Configuration.GetSection("Kestrel"))
-//        .Endpoint("HTTPS", listenOptions =>
-//    {
-//        listenOptions.HttpsOptions.SslProtocols = SslProtocols.Tls13;
-//    });
-//}));
-
-//string kestrelCertUrl = builder.Configuration.GetValue<string>("KestrelCertUrl")
-//    .Replace("%USERPROFILE%", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
-
-//string kestrelCertPassword = builder.Configuration.GetValue<string>("KestrelCertPassword");
-
-//builder.Configuration.GetSection("Kestrel:Endpoints:Https:Certificate:Path").Value = kestrelCertUrl;
-//builder.Configuration.GetSection("Kestrel:Endpoints:Https:Certificate:Password").Value = kestrelCertPassword;
-
-builder.Services.AddScoped<SymmetricalEncryption>();
-builder.Services.AddScoped<ASymmetricalEncryption>();
-builder.Services.AddScoped<HashingHandler>();
+// Extension methods for adding services
+builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddTodoDbContext(builder.Configuration);
+builder.Services.AddIdentityServices();
+builder.Services.AddEncryptionServices();
+builder.Services.AddAuthorizationPolicies();
 builder.Services.AddHttpClient();
+// See comment in DependencyInjection.cs why it's not used here
+//DependencyInjection.ConfigureKestrel(builder);
 
 var app = builder.Build();
 
@@ -97,7 +47,6 @@ else
 }
 
 app.UseHttpsRedirection();
-
 
 app.UseAntiforgery();
 
